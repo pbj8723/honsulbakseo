deaae66101dff55cf1d06456851dcc3fb52ebbd5
calf0530 <calf0530@naver.com>
2019-03-13 18:00:42 +0900

    add regist

--- app/src/main/java/cs/honsulbakeso/com/honsulbakseo/LoginActivity.java
+++ app/src/main/java/cs/honsulbakeso/com/honsulbakseo/LoginActivity.java
@@ -1,13 +1,28 @@
 package cs.honsulbakeso.com.honsulbakseo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView register_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register_btn=(TextView)findViewById(R.id.person_register);
+
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
