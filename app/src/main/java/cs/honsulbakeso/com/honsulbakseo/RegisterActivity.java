fbff0f66bfc9337bdc482e6bc938f54dd65337f8
hyebinppark <gpqls304@naver.com>
2019-03-13 19:53:31 +0900

    change design

--- app/src/main/java/cs/honsulbakeso/com/honsulbakseo/RegisterActivity.java
+++ app/src/main/java/cs/honsulbakeso/com/honsulbakseo/RegisterActivity.java
@@ -1,13 +1,28 @@
 package cs.honsulbakeso.com.honsulbakseo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button okbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        okbtn=(Button)findViewById(R.id.okbtn);
+
+        okbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplication(), recipeboardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
